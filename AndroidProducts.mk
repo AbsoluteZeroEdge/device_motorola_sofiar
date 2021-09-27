8c8e00e90a73908cb86acb64ee7a7ceca662fb53
ph4n70m <87523412+ph4n70m-404@users.noreply.github.com>
2021-09-26 23:59:24 -0500

    change to spark

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/havoc_sofiar.mk
+    $(LOCAL_DIR)/_sofiar.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_sofiar-user \
-    havoc_sofiar-userdebug \
-    havoc_sofiar-eng \
+    _sofiar-user \
+    _sofiar-userdebug \
+    _sofiar-eng \
