a1e7083f751ec09dcaff398368d8c597e4cb9055
ph4n70m <87523412+ph4n70m-404@users.noreply.github.com>
2021-10-30 02:01:42 -0500

    Update havoc_sofiar.mk

--- havoc_sofiar.mk
+++ havoc_sofiar.mk
@@ -31,7 +31,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common HavocOS stuff
+# Inherit some common Havoc-OS stuff
 $(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
